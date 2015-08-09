b144a126f5a8ed632ba85917334bf33f6f29d7bb
rlim <halim@abh.my>
2015-08-09 16:07:00 +0800

    add - patients_controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,20 @@
+class PatientsController < ApplicationController
+  
+  def reg_new_patient
+  end
+  
+  def search_patient
+  end
+  
+  def load_mykad_picture
+  end
+  
+  def print_detail_patient
+  end
+  
+  def patient_detail
+  end
+  
+  def search_patient_autosuggest
+  end
+end
