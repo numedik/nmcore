22843cdf16745b26a1ce060461252bbac6529e1e
rlim <halim@abh.my>
2015-08-09 16:56:21 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,17 @@ Rails.application.routes.draw do
     get ':lookupmodel' => "lookup#dblookup" 
   end
   
+  controller :patients do
+    get '/register-new-patient' => 'patients#register_new_patient'
+    get '/search-patient' => 'patients#search_patient'
+    get '/search-patient-autosuggest' => 'patients#search_patient_autosuggest'
+    get '/load-mykad-picture/:imagename' => 'patients#load_mykad_picture'
+    get '/print-detail-patient/:patientid' => 'patients#print_detail_patient'
+  end
+  
+  controller :treatments do
+    get '/register-new-treatment' => 'treatments#register_new_treatment'
+    get '/active-treatment-patient' => 'treatments#active_treatment_patient'
+    get '/list-treatment-patient' => 'treatments#list_past_treatment'
+  end
 end
