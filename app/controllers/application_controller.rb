3517e1a33c2d7a385df19cfab4686fe5ecdebbf5
Haziman Hashim <haziman@abh.my>
2015-09-21 14:41:33 +0800

    try fix doorkeeper ex handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,19 @@ class ApplicationController < ActionController::Base
 
   def doorkeeper_unauthorized_render_options(ex = nil)
   
-    puts ex.to_yaml
-    # FIXME: either need ES or Indexing
-    @reason ||= params[:access_token].nil? ? "Empty access_token" : nil
-    @reason ||= (access_token  = ActiveRecord::Base.establish_connection
-      .connection.execute("SELECT * FROM oauth_access_tokens WHERE token = '#{params[:access_token]}'")
-      .first).nil? ? "Invalid access_token" : nil
-    @reason ||= !access_token[6].nil? ? 
-      "access_token revoked" : nil
-    @reason ||= access_token[7] + access_token[5].minutes < Time.now ?
-      "access_token has expired" : nil
+    if ex.nil?
+      # FIXME: either need ES or Indexing
+      @reason ||= params[:access_token].nil? ? "Empty access_token" : nil
+      @reason ||= (access_token  = ActiveRecord::Base.establish_connection
+        .connection.execute("SELECT * FROM oauth_access_tokens WHERE token = '#{params[:access_token]}'")
+        .first).nil? ? "Invalid access_token" : nil
+      @reason ||= !access_token[6].nil? ? 
+        "access_token revoked" : nil
+      @reason ||= access_token[7] + access_token[5].minutes < Time.now ?
+        "access_token has expired" : nil
+    else 
+      @reason = ex.description
+    end
       
     @reason ||= "Unkown Error"
     { 
