76b022325bfed41c6f77561a49ff1dd6bb73d754
Haziman Hashim <haziman@abh.my>
2015-09-21 14:35:39 +0800

    try fix doorkeeper ex handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
 
-  def doorkeeper_unauthorized_render_options
+  def doorkeeper_unauthorized_render_options(ex)
   
     # FIXME: either need ES or Indexing
     @reason ||= params[:access_token].nil? ? "Empty access_token" : nil
