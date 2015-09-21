c7991c1eb7608e6a4c5ebe3d0a17aea2c601676c
Haziman Hashim <haziman@abh.my>
2015-09-21 14:31:16 +0800

    more detailed error message, in case of unauthorised

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,23 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
 
   def doorkeeper_unauthorized_render_options
-    {json: '{"status": "failure", "message":"401 Unauthorized"}'}
+  
+    # FIXME: either need ES or Indexing
+    @reason ||= params[:access_token].nil? ? "Empty access_token" : nil
+    @reason ||= (access_token  = ActiveRecord::Base.establish_connection
+      .connection.execute("SELECT * FROM oauth_access_tokens WHERE token = '#{params[:access_token]}'")
+      .first).nil? ? "Invalid access_token" : nil
+    @reason ||= !access_token[6].nil? ? 
+      "access_token revoked" : nil
+    @reason ||= access_token[7] + access_token[5].minutes < Time.now ?
+      "access_token has expired" : nil
+      
+    @reason ||= "Unkown Error"
+    { 
+      template: 'oauth2/unauthorized',
+      content_type: 'text/json',
+      status: 401
+    }
   end
 
 end
