949ccb8d7d33f35ad1ae11f8718fc35122f36c1b
Haziman Hashim <haziman@abh.my>
2015-10-11 14:17:40 +0800

    modal

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -16,7 +16,7 @@ class Inventory < ActiveRecord::Base
   belongs_to :drugtype
   belongs_to :drugstrength
   belongs_to :drugdosage
-  belongs_to :drougroute
+  belongs_to :drugroute
   belongs_to :drugfrequency
   belongs_to :drugduration
   belongs_to :drugprocedure
