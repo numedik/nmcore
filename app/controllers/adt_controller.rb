5a46c4e11cc3eda1eeb425255d51b5e626b10307
Haziman Hashim <haziman@abh.my>
2015-11-02 15:40:25 +0800

    add work order in active_treatment

--- app/controllers/adt_controller.rb
+++ app/controllers/adt_controller.rb
@@ -99,7 +99,8 @@ class AdtController < ApplicationController
     info = Hash.new
     info[:treatment] = tid
     info[:patient] = tid.patient
+    info[:workflow] = tid.workflows.active
 
-    render :json => tid.to_json({:include => [:patient,:plan,:patienttype, :treatmentnote, :discipline, :treatmentstat], :except => [:created_at,:updated_at] })
+    render :json => tid.to_json({:include => [:patient,:plan,:patienttype, :treatmentnote, :discipline, :treatmentstat, :workflow], :except => [:created_at,:updated_at] })
   end
 end
