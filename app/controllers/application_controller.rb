43c2979f5442bed8372f9a95511902d73779429e
Haziman Hashim <haziman@abh.my>
2015-11-21 20:40:40 +0800

    enable return to after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:redirect_uri] || super
+    # raise session[:user_return_to]
+    session[:user_return_to] || root_path
   end
 end
